ef541b182a3e9380ebb3fe9225220897272f8e00
andela-tolotin <temitope.olotin@andela.com>
2016-02-17 23:06:13 +0100

    Add a database handler class

---
+++ src/Database/DatabaseHandler.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @package  Laztopaz\potato-ORM
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+namespace Laztopaz\potatoORM;
+
+class DatabaseHandler {
+
+	/**
+	 * This is a constructor; a default method  that will be called automatically during class instantiation
+	 */
+	public function DatabaseHandler()
+	{
+
+	}
+
+	/**
+	 * This method create a record and store it in a table row
+	 * @params
+	 * @return
+	 */
+	public function create()
+	{
+
+	}
+
+	/**
+	 * This method retrieves record from a table
+	 * @params
+	 * @return
+	 */
+	public function read()
+	{
+
+	}
+
+	/**
+	 * This method updates record in a table row
+	 * @params
+	 * @return
+	 */
+	public function update()
+	{
+
+	}
+
+
+	/**
+	 * This method deletes a record  from a table row
+	 * @params int id
+	 * @return boolean true or false
+	 */
+	public function delete()
+	{
+
+	}
+
+}
