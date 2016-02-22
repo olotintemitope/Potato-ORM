57a061f06355b25533b5d0a4d608bf202aa89f76
andela-tolotin <temitope.olotin@andela.com>
2016-02-22 22:29:37 +0100

    Add a class that handles no record update exception

--- src/Exception/NoRecordUpdateException.php
+++ src/Exception/NoRecordUpdateException.php
@@ -12,7 +12,7 @@ use Exception;
 
 class NoRecordUpdateException extends  Exception {
 
-	public static function  noRecordUpdateException($message)
+	public static function noRecordUpdateException($message)
 	{
 		return new static($message);
 	}
