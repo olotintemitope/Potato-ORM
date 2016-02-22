c9614da952a179149182da6151ec8586b181ebed
andela-tolotin <temitope.olotin@andela.com>
2016-02-22 22:26:01 +0100

    Add an exception to check for empty array

---
+++ src/Exception/EmptyArrayException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package  Laztopaz\potato-ORM
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+namespace Laztopaz\potatoORM;
+
+use Exception;
+
+class EmptyArrayException extends Exception {
+
+	public static function emptyArrayException($message)
+	{
+		return new static($message);
+	}
+
+}
