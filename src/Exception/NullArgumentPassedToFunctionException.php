291ba95b67c08033b84661cad57fd1e304728a38
andela-tolotin <temitope.olotin@andela.com>
2016-02-22 23:26:45 +0100

    Change the filename

---
+++ src/Exception/NullArgumentPassedToFunctionException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andela
+ * Date: 2/22/16
+ * Time: 9:38 PM
+ */
+
+namespace Laztopaz\potatoORM;
+
+use Exception;
+
+class NullArgumentPassedToFunction  extends  Exception{
+
+	public static function nullArgumentPassedToFunction($message)
+	{
+		return new static ($message);
+	}
+
+}
