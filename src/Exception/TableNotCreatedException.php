f486604568c119f4c55a9c1296f208fbaef2da3f
andela-tolotin <temitope.olotin@andela.com>
2016-02-22 22:31:54 +0100

    Add a class that handles table not created exception

---
+++ src/Exception/TableNotCreatedException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package  Laztopaz\potato-ORM
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+namespace Laztopaz\potatoORM;
+
+use Exception;
+
+class TableNotCreatedException extends  Exception {
+
+	public static  function tableNotCreatedException($message)
+	{
+		return new static($message);
+	}
+
+}
