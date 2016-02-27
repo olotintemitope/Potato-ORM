73cb7027304993b0b67f667dc5907141d386f269
andela-tolotin <temitope.olotin@andela.com>
2016-02-27 08:51:52 +0100

    Add comments

--- test/CrudOperationTest.php
+++ test/CrudOperationTest.php
@@ -32,6 +32,12 @@ class TestCrudOperation extends PHPUnit_Framework_TestCase {
 		$this->dbHelper = new DatabaseHelper($this->dbConn);
 	}
 
+
+	/**
+	 * This method test if a record is commited to a particular table
+	 * @params void
+	 * @return boolean true
+	 */
 	public function testSave()
 	{
 		$this->user->name    = "Temitope Emmanuel";
@@ -43,6 +49,11 @@ class TestCrudOperation extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($this->user->save());
 	}
 
+	/**
+	 * This method test if a record is in a particular table and update it with a new if the record is found
+	 * @params void
+	 * @return boolean true
+	 */
 	public function testFind()
 	{
 		$user         = User::find(50);
@@ -52,12 +63,22 @@ class TestCrudOperation extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($user->save());
 	}
 
+	/**
+	 * This method test if a record is in a particular table and delete it if the record is found
+	 * @params void
+	 * @return boolean true
+	 */
 	public function testDestroy()
 	{
 		$boolResponse = User::destroy(46);
 		$this->assertTrue($boolResponse);
 	}
 
+	/**
+	 * This method gets all the records in a particular table
+	 * @params void
+	 * @return array $arrayOfUserRecords
+	 */
 	public function testGetAll()
 	{
 		$arrayOfUserRecords = User::getAll();
@@ -65,12 +86,22 @@ class TestCrudOperation extends PHPUnit_Framework_TestCase {
 		$this->assertGreaterThan(0,count($arrayOfUserRecords));
 	}
 
+	/**
+	 * This method checks if the data return is an array
+	 * @params void
+	 * @return boolean true
+	 */
 	public function testResultIfReturnDataIsArray()
 	{
 		$boolResponse = $this->user->checkIfRecordIsEmpty(['id' => 1, 'name' => 'Olotin Temitope', 'alias' => 'Laztopaz']);
 		$this->assertTrue($boolResponse);
 	}
 
+	/**
+	 * This method checks if the data return is an array and empty
+	 * @params void
+	 * @return boolean false
+	 */
 	public function testResultReturnIsEmptyArray()
 	{
 		$data = [];
@@ -78,11 +109,18 @@ class TestCrudOperation extends PHPUnit_Framework_TestCase {
 		$this->assertFalse($boolResponse);
 	}
 
+	/**
+	 * This method checks if the setter and getter matches the table fields
+	 * @params void
+	 * @return boolean true
+	 */
 	public function testCheckObjectRelatedMapping()
 	{
 		$expectedFields = $this->dbHelper->getColumnNames('users');
 
-		$fieldsFromMagicSetter = ['id' => 1, 'name' => 'Temitope', 'class' => '14'];
+		$fieldsFromMagicSetter = ['0' => 'id', '1' => 'name', '2' => 'gender','3' => 'alias', '4' => 'class', '5' => 'stack'];
+
+		$this->assertEquals($expectedFields,$fieldsFromMagicSetter);
 
 
 	}
