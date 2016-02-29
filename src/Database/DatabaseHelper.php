36b3449b9a875dfb57ee8b46d4854c24b11f08cb
Temitope Olotin <temitope.olotin@andela.com>
2016-02-29 01:25:26 +0100

    Update DatabaseHelper.php

--- src/Database/DatabaseHelper.php
+++ src/Database/DatabaseHelper.php
@@ -12,35 +12,33 @@ use PDO;
 use Laztopaz\potatoORM\TableNotCreatedException;
 
 class DatabaseHelper {
-
-	public $dbConn;
-
-	/**
-	 * This is a constructor; a default method  that will be called automatically during class instantiation
-	 */
-	public function __construct($dbConnect)
-	{
-		$this->dbConn = $dbConnect;
-	}
-
-	/**
-	 * This method creates a particular table
-	 * @param tableName
-	 * $return boolean true or false
-	 */
-	public function createTable($tableName, $conn = NULL)
-	{
-		if (is_null($conn)) {
-
-			$conn = $this->dbConn;
-		}
-		$sql = 'CREATE TABLE IF NOT EXISTS '.$tableName.'(';
-		$sql.= ' id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, name VARCHAR( 100 ), gender VARCHAR( 10 ), alias VARCHAR( 150 ) NOT NULL, class VARCHAR( 150 ), stack VARCHAR( 50 ) )';
-
-		return $conn->exec($sql);
-
-		throw TableNotCreatedException::tableNotCreatedException("Check your database connection");
-
-	}
-
+	
+    public $dbConn;
+    
+    /**
+      * This is a constructor; a default method  that will be called automatically during class instantiation
+      */
+    public function __construct($dbConnect)
+    {
+    	$this->dbConn = $dbConnect;
+    }
+    
+    /**
+      * This method creates a particular table
+      * @param tableName
+      * $return boolean true or false
+      */
+   public function createTable($tableName, $conn = NULL)
+   {
+   	if (is_null($conn)) {
+   	    $conn = $this->dbConn;
+   	}
+   	
+   	$sql = 'CREATE TABLE IF NOT EXISTS '.$tableName.'(';
+   	$sql.= ' id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, name VARCHAR( 100 ), gender VARCHAR( 10 ), alias VARCHAR( 150 ) NOT NULL, class VARCHAR( 150 ), stack VARCHAR( 50 ) )';
+   	
+   	return $conn->exec($sql);
+   	throw TableNotCreatedException::tableNotCreatedException("Check your database connection");
+   }
+	
 }
