f2755df9682a0076601a58bdae8a534215326859
Temitope Olotin <temitope.olotin@andela.com>
2016-02-29 01:36:00 +0100

    Update InterfaceBaseClass.php

--- src/Interface/InterfaceBaseClass.php
+++ src/Interface/InterfaceBaseClass.php
@@ -10,32 +10,31 @@ namespace Laztopaz\potatoORM;
 
 interface InterfaceBaseClass {
 
-	/**
-	 * This method gets all the record from a particular table
-	 * @params void
-	 * @return associative array
-	 */
-	public static function getAll();
-
-	/**
-	 * This method create or update record in a database table
-	 * @params void
-	 * @return boolean true or false;
-	 */
-	public function save();
-
-	/**
-	 * This method delete a row from the table by the row id
-	 * @params int $id
-	 * @return boolean true or false
-	 */
-	public static function destroy($id);
-
-
-	/**
-	 * This method find a record by id
-	 * @params int $id
-	 * @return object find
-	 */
-	public static function find($id);
+    /**
+     * This method gets all the record from a particular table
+     * @params void
+     * @return associative array
+     */
+   public static function getAll();
+   
+   /**
+    * This method create or update record in a database table
+    * @params void
+    * @return boolean true or false;
+    */
+   public function save();
+   
+   /**
+    * This method delete a row from the table by the row id
+    * @params int $id
+    * @return boolean true or false
+    */
+     public static function destroy($id);
+     
+   /**
+    * This method find a record by id
+    * @params int $id
+    * @return object find
+    */
+   public static function find($id);
 }
