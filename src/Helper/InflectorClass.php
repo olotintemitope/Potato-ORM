0167cad78aa4f247e256ba6b24474475975fbd08
andela-tolotin <temitope.olotin@andela.com>
2016-03-01 10:09:40 +0100

    Add an inflector class

---
+++ src/Helper/InflectorClass.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package  Laztopaz\potato-ORM
+ * @author   Temitope Olotin <temitope.olotin@andela.com>
+ * @license  <https://opensource.org/license/MIT> MIT
+ */
+
+namespace Laztopaz\potatoORM
+
+class Inflector {
+
+	use Inflector;
+}
